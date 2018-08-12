8bf3d2c519f26d59779c5550c20c2c86d4d371fa
DESKTOP-RHDUPBO\azb <azbatuk@gmail.com>
2018-08-11 20:22:17 -0700

    RectF added

---
+++ Engine/RectF.cpp
@@ -0,0 +1,34 @@
+#include "RectF.h"
+
+RectF::RectF(float left_in, float top_in, float right_in, float bottom_in)
+	:
+	left(left_in),
+	top(top_in),
+	right(right_in),
+	bottom(bottom_in)
+{
+}
+
+RectF::RectF(const Vec2 & topLeft, const Vec2 & bottomRight)
+	:
+	RectF(topLeft.x, topLeft.y, bottomRight.x, bottomRight.y)
+{
+}
+
+RectF::RectF(const Vec2 & topLeft, float width_in, float height_in)
+	:
+	RectF(topLeft, Vec2(topLeft.x + width_in, topLeft.y + height_in))
+{
+}
+
+bool RectF::isRectOverlapping(const RectF& other) const
+{
+	if (left   < other.right &&
+		right  > other.left &&
+		top    < other.bottom &&
+		bottom > other.top)
+	{
+		return true;
+	}
+	return false;
+}
---
+++ Engine/RectF.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Vec2.h"
+
+class RectF
+{
+public:
+	RectF(float left_in, float top_in, float right_in, float bottom_in);
+	RectF(const Vec2& topLeft, const Vec2& bottomRight);
+	RectF(const Vec2& topLeft, float width_in, float height_in);
+	bool isRectOverlapping(const RectF& other) const;
+public:
+	float left;
+	float top;
+	float right;
+	float bottom;
+};
